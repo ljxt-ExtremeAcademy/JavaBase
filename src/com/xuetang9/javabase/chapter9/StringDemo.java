7760483b4e76f9b55c0e7cedeb1897d8ea95b822
窖头 <@DESKTOP-PJ6BUH1>
2017-07-09 17:41:28 +0800

    增加关于字符串常量池的讲解

--- src/com/xuetang9/javabase/chapter9/StringDemo.java
+++ src/com/xuetang9/javabase/chapter9/StringDemo.java
@@ -10,11 +10,23 @@ package com.xuetang9.javabase.chapter9;
  */
 public class StringDemo {
 	public static void main(String[] args) {
-		String value = "Hello World";
-		value += "Good";	//相当于调用了value.concat()方法，会直接返回一个新的字符串对象空间
-		String value2 = "Hello WorldGood";//将字符串池中的地址赋给value2
+		String 刘强东老婆 = "龚小京";	//龚小京这个字符串就被存放在了字符串池中
+		刘强东老婆 = "奶茶妹妹";			//奶茶妹妹 也是在字符串池中的！
+		//new String("奶茶妹妹@小强东")
+		刘强东老婆 = 刘强东老婆.concat("@小强东");	//注意：刘强东的老婆已经不是原来在常量池中的奶茶妹妹了！
+		//思考：奶茶妹妹@小强东 是否在常量池中？？？？
+		System.out.println(刘强东老婆);
 		
-		System.out.println(value.equals(value2));
+		String 刘强东前妻 = "龚小京";
+		
+		
+//		String value = "Hello World";
+//		String value1 = value;
+//		value += "Good";	//相当于调用了value.concat()方法，会直接返回一个新的字符串对象空间
+//		String value2 = "Hello WorldGood";//将字符串池中的地址赋给value2
+//		
+//		System.out.println(value);
+//		System.out.println(value1);
 		
 //		String value = "中国人民解放军";
 //		//value += "老牛鼻子了！";
